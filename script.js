580f141c0bd8088cc57d94ab02161320e8d43cff
Lav Yadav <yadavlav571@gmail.com>
2024-04-28 21:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const byteSize = (str) => {
-  // write your code here
+let byteSize = 0;
+  for (let i = 0; i < str.length; i++) {
+    const codePoint = str.charCodeAt(i);
+
+    // Basic ASCII characters (0-127) take up 1 byte each
+    if (codePoint < 128) {
+      byteSize += 1;
+    }
+    // Extended ASCII characters (128-255) take up 2 bytes each
+    else if (codePoint >= 128 && codePoint <= 255) {
+      byteSize += 2;
+    }
+    // Other Unicode characters (including multi-byte UTF-8 characters)
+    else {
+      byteSize += 3;
+    }
+  }
+  return byteSize;
+      
 };
 
 // Do not change the code below
